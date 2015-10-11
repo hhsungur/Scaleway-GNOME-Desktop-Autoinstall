0b8084433e741f036452dcbbcb1d8c4e7ff64b3f
Hasan Hüseyin Sungur <hasan.huseyin.sungur@ogrenci.karatay.edu.tr>
2015-10-11 22:09:27 +0300

    Ubuntu 14.04 Desktop Install

---
+++ ubuntu-14.04.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+read -p "Port: " -e -i 1 PORT
+read -p "Rosolution: " -e -i 1280x720 RESOLUTION
+
+apt-get --yes update
+
+cd
+apt-get --yes install vnc4server
+vncserver :$port
+
+apt-get --yes install firefox chromium-browser
+apt-get --yes install xfce4
+apt-get --yes install gnome-core
+
+cd ~/.vnc/
+mv xstartup xstartup.bak
+wget https://raw.githubusercontent.com/hhsungur/gnome-desktop-autoinstall/master/xstartup --no-check-certificate
+chmod +x xstartup
+
+cd /etc/init.d/
+wget https://raw.githubusercontent.com/hhsungur/gnome-desktop-autoinstall/master/vncserver --no-check-certificate
+chmod +x vncserver
+
+mkdir -p /etc/vncserver
+cd /etc/vncserver/
+touch vncservers.conf
+echo 'VNCSERVERS="'$PORT':root"' | tee -a /etc/vncserver/vncservers.conf
+echo 'VNCSERVERARGS[1]="-geometry '$RESOLUTION'"' | tee -a /etc/vncserver/vncservers.conf
+update-rc.d vncserver defaults 99
+reboot
